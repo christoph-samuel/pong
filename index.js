3ab6a789050bfbe8d2ef1e3361853dd2b317d0e6
Christoph Samuel <chsamuel@gmx.at>
2022-04-06 14:46:25 +0200

    Chat Bot

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    // console.log('a user connected');
+    io.emit('chat message', "--- A user connected ---");
+    socket.on('disconnect', () => {
+        // console.log('user disconnected');
+        io.emit('chat message', "--- A user disconnected ---");
+    });
+    socket.on('chat message', (msg) => {
+        // console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
